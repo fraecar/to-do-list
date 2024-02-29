02bb90a615591689ba9e8b18c48299affe617658
fraecar <galaxie.fraecar.fire@gmail.com>
2024-02-29 17:00:24 +0100

    v0.0.2
    sauvegarde de la liste dans le localstorage du navigateur

--- script.js
+++ script.js
@@ -1,10 +1,33 @@
 let inputToDojs = document.getElementById("inputToDo");
 let boutonADDjs = document.getElementById("boutonADD");
+let i = Number(1)
+
+function setI(KeyIs){
+    testkeyIs = localStorage.getItem(KeyIs)
+    if (testkeyIs == null) {
+        localStorage.setItem(KeyIs, 1)
+    }
+    else {
+        i = localStorage.getItem(KeyIs)
+    }
+}
+
+function addTolist(){
+    boutonADDjs.addEventListener("click", function(){
+        let texte = inputToDojs.value
+        let newli = document.createElement('li')
+        i = i + 1
+        localStorage.setItem(i, texte)
+        newli.textContent = texte
+        document.getElementById("ListeToDo").appendChild(newli)
+    });
+}
+
+function boucle(){
+    setI()
+    addTolist()
+}
+
+boucle()
 
 
-boutonADDjs.addEventListener("click", function(){
-    let texte = inputToDojs.value
-    let newli = document.createElement('li')
-    newli.textContent = texte
-    document.getElementById("ListeToDo").appendChild(newli)
-})

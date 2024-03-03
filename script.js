ab38de97527b4046154c58bd954d8339935ad760
fraecar <galaxie.fraecar.fire@gmail.com>
2024-03-03 15:50:52 +0100

    v0.0.6

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 let inputToDojs = document.getElementById("inputToDo");
 let boutonADDjs = document.getElementById("boutonADD");
+let suppButtonjs = document.getElementsByClassName("suppButton")
 
 function addToListStart(){
     for(i = 1; i <= localStorage.length; i++) {
         let newli = document.createElement("li")
         newli.textContent = localStorage.getItem(i)
+        newli.setAttribute("id", i)
         document.getElementById("ListeToDo").appendChild(newli)
     }
 }
@@ -16,6 +18,7 @@ function addTolist(){
         key = localStorage.length + 1
         localStorage.setItem(key, texte)
         newli.textContent = texte
+        newli.setAttribute("id", key)
         document.getElementById("ListeToDo").appendChild(newli)
     });
 }
